3ea678a5b62456e3398119b7dee86224e2909dfe
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-10-30 18:39:05 -0300

    feat: add update and delete article routes

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@
 
 class Article < ApplicationRecord
   validates :title, :body, presence: true
-  validates :title, length: { minimum: 6, maximum: 36 }
-  validates :body, length: { minimum: 30, maximum: 1000 }
+  validates :title, length: { minimum: 6, maximum: 100 }
+  validates :body, length: { minimum: 30, maximum: 3000 }
 end
