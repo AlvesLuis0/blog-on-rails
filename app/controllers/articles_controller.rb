d6b3d6697d6f360a443124a2c4376919a48bfab2
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-11-09 14:06:05 -0300

    fix: nah

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  before_action :authenticate_user!, except: %i[new create edit update destroy]
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
 
   def index
     @articles = Article.all
